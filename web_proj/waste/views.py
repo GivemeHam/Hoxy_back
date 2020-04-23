0a636ddc63aa0ef45148a867046d47a342b6f34f
HJY <wndyd5577@naver.com>
2020-04-23 14:28:14 +0900

    update db form to dictionary

--- web_proj/waste/views.py
+++ web_proj/waste/views.py
@@ -8,13 +8,21 @@ def home(request):
     return HttpResponse("Hello, Django!")
 
 def select_waste_type(request):
-    results = waste_type.objects.all()
+    #results = waste_type.objects.all()
+    result = waste_type.objects.filter(waste_type(waste_type_name='cjdthrl'))
     rst = waste_type.objects.order_by('waste_type_no')[0:5]
-    str = ''
+    
+    list = []
     for rst in results:
-        str += "{}{}{}{}{}{}{}".format(rst.waste_type_no, rst.waste_type_waste_div_no, rst.waste_type_name, rst.waste_type_kor_name, rst.waste_type_size, rst.waste_type_fee, rst.waste_type_area_no)
+        dic = {}
+        dic['waste_type_no'] = rst.waste_type_no
+        dic['waste_type_waste_div_no'] = rst.waste_type_waste_div_no
+        dic['waste_type_name'] = rst.waste_type_name
+        dic['waste_type_kor_name'] = rst.waste_type_kor_name
+        dic['waste_type_size'] = rst.waste_type_size
+        dic['waste_type_fee'] = rst.waste_type_fee
+        dic['waste_type_area_no'] = rst.waste_type_area_no
+        list.append(dic)
         
-
-    #result = waste_type.objects.filter(waste_type(waste_type_name='cjdthrl') & waste_type(waste_type_area=1))
-    context = {'result_value':str}
+    context = {'result_value':list}
     return render(request, 'waste_db/waste_type.html', context )
